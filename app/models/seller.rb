481e305995d4307241d4714ecaee5e998f830f1a
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-03-31 19:56:02 -0600

    Refactored new Seller model.
    We will have many different methods on the seller. We implemented the seller.name and fixed it in the spec.

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,3 +1,7 @@
 class Seller < ActiveRecord::Base
   has_one :user, as: :userable
+
+  def name
+    self.user.username
+  end
 end
--- spec/features/seller_dashboard_spec.rb
+++ spec/features/seller_dashboard_spec.rb
@@ -9,6 +9,6 @@ describe 'seller viewing dashboard', type: :feature do
     fill_in "session[password]", with: user.password
     click_link_or_button("log in")
     visit "/#{seller.slug}/dashboard"
-    expect(page).to have_content(seller.user.username)
+    expect(page).to have_content(seller.name)
   end
 end
