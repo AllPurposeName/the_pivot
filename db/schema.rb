22e2a34ae44cfbae05d8bfa6e395ae43eba1750d
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-04-14 13:28:21 -0600

    Set user role default to 'default'
    This was causing some problems using the sign in function. Proper tests would have caught this had we the time to robustify them.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150412062541) do
+ActiveRecord::Schema.define(version: 20150414192418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,11 +70,11 @@ ActiveRecord::Schema.define(version: 20150412062541) do
   create_table "users", force: :cascade do |t|
     t.text     "username"
     t.string   "password_digest"
-    t.integer  "role"
+    t.integer  "role",            default: 0
     t.text     "full_name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.integer  "userable_id"
     t.string   "userable_type"
   end
