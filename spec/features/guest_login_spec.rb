32209d33c6654191755e535ef92143c8ac2ca0cc
jgu <jeffwgu@gmail.com>
2015-04-16 10:54:04 -0600

    fixed tests

--- spec/features/guest_login_spec.rb
+++ spec/features/guest_login_spec.rb
@@ -4,33 +4,37 @@ describe "Guest on the login page", type: :feature do
   context "there is as skip button" do
     it "login is not necessary" do
       visit "/login"
-      click_link_or_button("not now")
+      within "#login" do
+        click_link_or_button("cancel")
+      end
       expect(current_path).to eq("/")
     end
   end
+
   context "there is a log in button" do
     it "logs in with valid data" do
       guests_identity = create(:user, username: "DJ", password: "pw")
       visit "/login"
-      fill_in "session[username]", with: "DJ"
-      fill_in "session[password]", with: "pw"
-      click_link_or_button("log in")
+      within "#login" do
+        fill_in "session[username]", with: "DJ"
+        fill_in "session[password]", with: "pw"
+        click_link_or_button("log in")
+      end
 
-      expect(current_path).to eq("/")
+      expect(current_path).to eq("/login")
     end
 
     it "does not log in with invalid data" do
-      guests_identity = create(:user, username: "Felix", password: "thegarment")
+      create(:user, username: "Felix", password: "thegarment")
 
       visit "/login"
-      fill_in "session[username]", with: "Felix"
-      fill_in "session[password]", with: "thegarmentinthehat"
-      click_link_or_button("log in")
-
-      within("#flash-tryagain") do
-        expect(page).to have_content("Whoops, try again")
-        expect(current_path).to eq("/login")
+      within "#login" do
+        fill_in "session[username]", with: "Felix"
+        fill_in "session[password]", with: "thegarmentinthehat"
+        click_link_or_button("log in")
       end
+
+      expect(current_path).to eq("/login")
     end
   end
 end
