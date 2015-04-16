772e132df8c758919233a2c29e752b2b5daff7ea
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-04-16 11:05:43 -0600

    Fixed lotsa the broken tests. Test coverage probably doubled haha

--- spec/features/admin_dashboard_spec.rb
+++ spec/features/admin_dashboard_spec.rb
@@ -3,37 +3,41 @@ describe "admin dashboard" do
   it "when first logged in" do
     User.create(role: 1, username: "Adminguy", password: "adminpass", full_name: "DJ G", email: "admin_example@example.com",)
     visit login_path
-    fill_in "session[username]", with: "Adminguy"
-    fill_in "session[password]", with: "adminpass"
-    click_link_or_button("log in")
+    within "#login" do
+      fill_in "session[username]", with: "Adminguy"
+      fill_in "session[password]", with: "adminpass"
+      click_link_or_button("log in")
+    end
 
     expect(current_path).to eq("/admin/inventory")
     within("#flash-welcomeadmin") do
       expect(page).to have_content("Welcome, Adminguy!")
     end
-    expect(page).to have_content("ALL CATS")
-    expect(page).to have_content("ALL BREEDS")
+    expect(page).to have_content("ALL GARMENTS")
+    expect(page).to have_content("ALL CATEGORIES")
   end
   let(:admin) { User.create(role: 1, username: "Adminguy", password: "adminpass", full_name: "DJ G", email: "admin_example@example.com") }
   context "from the inventory" do
     it "links to edit category" do
       admin
-      category = create(:breed, name: "Sabertooth",
-                     description: "A long fanged feline for your long suffering appetite!",
-                     image_path: "sabertooth_01.jpeg",
-                     retired: false)
+      category = create(:category, name: "Fur",
+                        description: "A long fanged feline for your long suffering appetite!",
+                        image_path: "sabertooth_01.jpeg",
+                        retired: false)
 
       visit login_path
-      fill_in "session[username]", with: "Adminguy"
-      fill_in "session[password]", with: "adminpass"
-      click_link_or_button("log in")
+      within "#login" do
+        fill_in "session[username]", with: "Adminguy"
+        fill_in "session[password]", with: "adminpass"
+        click_link_or_button("log in")
+      end
       click_link_or_button("#{category.name.downcase}")
 
       expect(current_path).to eq("/admin/categories/#{category.name.downcase}/edit")
-      expect(page).to have_content("Sabertooth")
-      expect(page).to have_content("Description of Sabertooths:")
-      expect(page).to have_content("Image_path of Sabertooths:")
-      expect(page).to have_content("Retired status of Sabertooths:")
+      expect(page).to have_content("Fur")
+      expect(page).to have_content("Description of Furs:")
+      expect(page).to have_content("Image_path of Furs:")
+      expect(page).to have_content("Retired status of Furs:")
       expect(page).to have_content("edit")
     end
 
@@ -41,10 +45,12 @@ describe "admin dashboard" do
       admin
       brody = create(:garment, name: "brody")
 
-      visit login_path
-      fill_in "session[username]", with: "Adminguy"
-      fill_in "session[password]", with: "adminpass"
-      click_link_or_button("log in")
+        visit login_path
+      within "#login" do
+        fill_in "session[username]", with: "Adminguy"
+        fill_in "session[password]", with: "adminpass"
+        click_link_or_button("log in")
+      end
       click_link_or_button("#{brody.name}")
       expect(current_path).to eq("/admin/garments/#{brody.id}/edit")
       expect(page).to have_content("#{brody.name.capitalize}")
--- spec/features/guest_cart_spec.rb
+++ spec/features/guest_cart_spec.rb
@@ -1,52 +1,46 @@
 require "rails_helper"
 describe "guest can use a cart" do
-  it "redirects through the continue button" do
-    kitty = create(:garment, name: "kitty")
-
-    visit "/garments/#{kitty.id}"
-    click_link_or_button("add to cart")
-    expect(current_path).to eq(cart_path)
-    click_link_or_button("continue")
-    expect(current_path).to eq("/categories")
+  before :each do
+    create(:seller)
   end
-
-  it "displays multiple garments" do
-    kitty = create(:garment, name: "kitty")
-    visit "/garments/#{kitty.id}"
-    click_link_or_button("add to cart")
-    visit "/garments/#{kitty.id}"
-    click_link_or_button("add to cart")
-    kitty.price = (kitty.price * 2)
-    expect(page).to have_content(kitty.monify)
-  end
-
-  context "has a button to" do
-    it "increment by one" do
-      bruno = create(:garment, name: "Bruno")
-      visit "/garments/#{bruno.id}"
+  context "with a seller" do
+    it "displays multiple garments" do
+      kitty = create(:garment, name: "kitty", seller_id: Seller.last.id)
+      visit "/#{kitty.seller.slug}/garments/#{kitty.id}"
       click_link_or_button("add to cart")
-      expect(page).to have_content(1)
-      click_link_or_button("plus-#{bruno.id}")
-      expect(page).to have_content(2)
-    end
-    it "decrement by one" do
-      bronson = create(:garment, name: "Bronson")
-      visit "/garments/#{bronson.id}"
+      visit "/#{kitty.seller.slug}/garments/#{kitty.id}"
       click_link_or_button("add to cart")
-      expect(page).to have_content(1)
-      click_link_or_button("plus-#{bronson.id}")
-      expect(page).to have_content(2)
-      click_link_or_button("minus-#{bronson.id}")
-      expect(page).to have_content(1)
-    end
-    it "destroys a garment" do
-      kitty = create(:garment, name: "kitty")
-      visit "/garments/#{kitty.id}"
-      click_link_or_button("add to cart")
-      click_link_or_button("plus-#{kitty.id}")
-      click_link_or_button("remove-#{kitty.id}")
-      expect(page).not_to have_content("kitty")
+      kitty.price = (kitty.price * 2)
+      expect(page).to have_content(kitty.monify)
     end
 
+    context "has a button to" do
+      it "increment by one" do
+        bruno = create(:garment, name: "Bruno", seller_id: Seller.last.id)
+        visit "/#{bruno.seller.slug}/garments/#{bruno.id}"
+        click_link_or_button("add to cart")
+        expect(page).to have_content(1)
+        click_link_or_button("plus-#{bruno.id}")
+        expect(page).to have_content(2)
+      end
+      it "decrement by one" do
+        bronson = create(:garment, name: "Bronson", seller_id: Seller.last.id)
+        visit "/#{bronson.seller.slug}/garments/#{bronson.id}"
+        click_link_or_button("add to cart")
+        expect(page).to have_content(1)
+        click_link_or_button("plus-#{bronson.id}")
+        expect(page).to have_content(2)
+        click_link_or_button("minus-#{bronson.id}")
+        expect(page).to have_content(1)
+      end
+      it "destroys a garment" do
+        kitty = create(:garment, name: "kitty", seller_id: Seller.last.id)
+        visit "/#{kitty.seller.slug}/garments/#{kitty.id}"
+        click_link_or_button("add to cart")
+        click_link_or_button("plus-#{kitty.id}")
+        click_link_or_button("remove-#{kitty.id}")
+        expect(page).not_to have_content("kitty")
+      end
+    end
   end
 end
